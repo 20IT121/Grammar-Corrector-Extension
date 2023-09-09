224b535d3ca5a8e1db312fe502ff23999529a7aa
20IT121 <20IT121@charusat.edu.in>
2023-09-09 10:54:52 +0530

    Extension--v-2

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+from flask import Flask, requests, jsonify
 import openai
-import backoff
 
-# @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
+app = Flask(__name__)
+openai.api_key = "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"
+
+@app.route('/generate-text', methods=['POST'])
 def correct_grammar(text):
     # openai.api_key = "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"
     
@@ -9,21 +12,34 @@ def correct_grammar(text):
     #     model="gpt-3.5-turbo-0301",
     #     messages=[{"role": "user", "content": "Correct the grammar of following sentence: '{text}'\n"}]
     # )
+    try:
+        # GET user_input from POST request
+        data = requests.get_json()
+        user_input = data.get('input', '')
 
+        # Make a request to openAI 
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Correct the grammar of following sentence: '{user_input}'\n",
+            n=1,
+            max_tokens=50
+        )
 
-    openai.api_key = "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"
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Correct the grammar of following sentence: '{text}'\n",
-        n=1,
-        max_tokens=50
-    )
+        # Extracting generated text from response
+        corrected_text = response.choices[0].text.strip()
 
-    corrected_text = response.choices[0].text.strip()
-    return corrected_text
+        # Send the generated text as JSON response
+        return jsonify({'generated-text' : corrected_text})
+    
+    except Exception as e:
+        return jsonify({'error' : str(e)}) , 500
+    
 
-# Example usage
-input_text = "He were an good boy."
-output_text = correct_grammar(input_text)
-print(f"Original: {input_text}")
+# # Example usage
+# input_text = "He were an good boy."
+output_text = correct_grammar()
+print(f"Original: ")
 print(f"Corrected: {output_text}")
+
+if __name__ == '__main__':
+    app.run(debug=True)
